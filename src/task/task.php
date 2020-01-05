6a3a674e239f5c22512003a075ac90e0b2a427d3
igor193php <iigor193@yandex.ru>
2020-01-05 17:15:22 +0300

    added class and methods in it

--- src/task/task.php
+++ src/task/task.php
@@ -1,26 +1,69 @@
 <?php
 
 class Task {
-
+   
     const STATUS_NEW = 'new';
-    const STATUS_CENCEL = 'cencel';
+    const STATUS_CENCELED = 'cenceled';
     const STATUS_INWORK = 'inwork';
     const STATUS_COMPLETED = 'completed';
     const STATUS_FAILED = 'failed';
 
-    const ACTION_PUBLISH = 'publish';
-    const ACTION_RESPOND = 'respond';
-    const ACTION_REFUSE = 'refuse';
-    const ACTION_COMPLETE = 'complete';
+    const ACTION_CENCEL = 'action_cencel';
+    const ACTION_RESPOND = 'action_respond';
+    const ACTION_REFUSE = 'action_refuse';
+    const ACTION_COMPLETE = 'action_complete';
 
-    public $idPerfomer = '';
-    public $idCustomer = '';
+    const ROLE_USER_EXECUTOR = 'executor';
+    const ROLE_USER_СUSTOMER = 'customer';
+   
+    
+    // public $idExecuter;
+    // public $idCustomer;
+    // public static $currentStatus = STATUS_NEW;  
 
-    public function __construct($idPerfomer, $idCustomer) {
-        $this->idPerfomer = $idPerfomer;
-        $this->idCustomer = $idCustomer;
+    // public function __construct(int $idExecuter, int $idCustomer) {
+    //     $this->idPerfomer = $idPerfomer;
+    //     $this->idCustomer = $idCustomer;        
+    // }
+
+    public function getMapOfStatus() {
+        $this->$mapStatus = [
+           self::STATUS_NEW => 'Новое',
+           self::STATUS_CENCELED => 'Отменено',
+           self::STATUS_INWORK => 'В работе',
+           self::STATUS_COMPLETED => 'Выполнено',
+           self::STATUS_FAILED => 'Провалено'
+        ];
+
+        return $mapStatus;
+    }
+
+    public function getMapOfActions() {
+        $this->$mapActions = [
+          self::ACTION_CENCEL => 'Отменить',
+          self::ACTION_RESPOND => 'Откликнуться',
+          self::ACTION_REFUSE => 'Отказаться',
+          self::ACTION_COMPLETE => 'Выполненно'
+        ];
+
+        return $mapActions;
+    }
+
+    public function getNextStatus() {
+        Task::getMapOfStatus();
+
+        return $nextStatus;
+        
+    }
+
+    public function getAvailableActions() {
+        Task::getMapOfActions();
+
+        return $availableActions;
+    }
 
     }
 
-
-}
+    $test = new Task;
+    $test->getMapOfActions();
+    print_r($test);
