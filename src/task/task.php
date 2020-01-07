5081d55ae714e3537caac49a318765558bc79821
igor193php <iigor193@yandex.ru>
2020-01-08 00:37:16 +0300

    rewrite all code

--- src/task/task.php
+++ src/task/task.php
@@ -1,7 +1,7 @@
 <?php
 
 class Task {
-   
+
     const STATUS_NEW = 'new';
     const STATUS_CENCELED = 'cenceled';
     const STATUS_INWORK = 'inwork';
@@ -15,19 +15,28 @@ class Task {
 
     const ROLE_USER_EXECUTOR = 'executor';
     const ROLE_USER_СUSTOMER = 'customer';
-   
-    
-    // public $idExecuter;
-    // public $idCustomer;
-    // public static $currentStatus = STATUS_NEW;  
 
-    // public function __construct(int $idExecuter, int $idCustomer) {
-    //     $this->idPerfomer = $idPerfomer;
-    //     $this->idCustomer = $idCustomer;        
-    // }
+
+    public $idExecute;
+    public $idCustomer;
+    public $currentStatus;
+    public $currentAction;
+    public $mapStatus;
+    public $mapActions;
+
+    public function __construct($idUser) {
+        if ($idUser == 1) {
+            $this->idExecute = $idUser;
+        }
+        elseif ($idUser == 2) {
+            $this->idCustomer = $idUser;
+        }
+
+        $this->currentStatus =  self::STATUS_NEW;
+    }
 
     public function getMapOfStatus() {
-        $this->$mapStatus = [
+        $this->mapStatus = [
            self::STATUS_NEW => 'Новое',
            self::STATUS_CENCELED => 'Отменено',
            self::STATUS_INWORK => 'В работе',
@@ -35,35 +44,70 @@ class Task {
            self::STATUS_FAILED => 'Провалено'
         ];
 
-        return $mapStatus;
+        return $this->mapStatus;
     }
 
     public function getMapOfActions() {
-        $this->$mapActions = [
+        $this->mapActions = [
           self::ACTION_CENCEL => 'Отменить',
           self::ACTION_RESPOND => 'Откликнуться',
           self::ACTION_REFUSE => 'Отказаться',
           self::ACTION_COMPLETE => 'Выполненно'
         ];
 
-        return $mapActions;
+        return $this->mapActions;
     }
 
-    public function getNextStatus() {
-        Task::getMapOfStatus();
+    // класс получает действие - возвращает статус который возможен после полученного действия
+    public function getNextStatus($currentAction) {
+        $this->currentAction = $currentAction;
+        if ($this->currentStatus == self::STATUS_NEW) {
+            if ($this->currentAction == self::ACTION_CENCEL) {
+                $this->currentStatus = self::STATUS_CENCELED;
+            }
+            elseif ($this->currentAction == self::ACTION_RESPOND) {
+                $this->currentStatus = self::STATUS_INWORK;
+            }
+        }
+        elseif ($this->currentStatus == self::STATUS_INWORK) {
+            if ($this->currentAction == self::STATUS_COMPLETED) {
+                $this->currentStatus = self::STATUS_COMPLETED;
+            }
+            elseif ($this->currentAction == self::ACTION_REFUSE) {
+                $this->currentStatus = self::STATUS_FAILED;
+            }
+        }
 
-        return $nextStatus;
-        
+        return $this->currentStatus;
+
     }
 
-    public function getAvailableActions() {
-        Task::getMapOfActions();
+    // класс получает статус - возращает доступные действия для полученного статуса
+    public function getAvailableActions($currentStatus) {
+        $this->currentStatus = $currentStatus;
+        if ($this->currentStatus == self::STATUS_NEW) {
+            if ($this->idCustomer) {
+                $this->currentAction = self::ACTION_CENCEL;
+            }
+            elseif ($this->idExecute) {
+                $this->currentAction = self::ACTION_RESPOND;
+            }
+        }
+        elseif ($this->currentStatus == self::STATUS_INWORK) {
+            if ($this->idCustomer) {
+                $this->currentAction = self::ACTION_COMPLETE;
+            }
+            elseif ($this->idExecute) {
+                $this->currentAction = self::ACTION_REFUSE;
+            }
+        }
 
-        return $availableActions;
+        return $this->currentAction;
     }
 
     }
 
-    $test = new Task;
-    $test->getMapOfActions();
-    print_r($test);
+$task = new Task(1);
+echo($task->getNextStatus('action_respond'));
+echo($task->getNextStatus('action_refuse'));
+
